7642fe5d2eb1f1d9a6a0e677453ef686e8e55ffb
Gabriel M Martins <gabrielmmartinscontato@gmail.com>
2023-02-23 16:11:07 -0300

    created task struct with default impl

--- src/database.rs
+++ src/database.rs
@@ -1,6 +1,6 @@
 use std::io;
 
-pub struct Task {}
+use self::task::Task;
 
 pub struct Database {
     tasks: Vec<Task>
@@ -27,5 +27,55 @@ impl Database {
 
     pub fn remove(&self, index: i64) {
         unimplemented!()
+    }
+}
+
+pub mod task {
+    use chrono::{DateTime, Local};
+
+    pub struct Task {
+        pub completed: bool,
+        pub priority: Option<char>,
+        pub dates: Option<Dates>,
+        pub desc: Description
+    }
+
+    pub struct Description {
+        pub txt: String,
+        pub tags: Vec<Tags>
+    }
+
+    pub enum Tags {
+        Project(String),
+        Context(String),
+        Special((String, String))
+    }
+
+    pub struct Dates {
+        pub creation: DateTime<Local>,
+        pub completed: Option<DateTime<Local>>,
+    }
+
+    impl Default for Description {
+        fn default() -> Self {
+            Description { txt: String::new(), tags: vec![] }
+        }
+    }
+
+    impl Default for Task {
+        fn default() -> Self {
+            Task {
+                completed: false,
+                priority: None,
+                dates: None,
+                desc: Description::default()
+            }
+        }
+    }
+
+    impl Task {
+        pub fn parse(txt: impl Into<String>) -> Self {
+            unimplemented!()
+        }
     }
 }
